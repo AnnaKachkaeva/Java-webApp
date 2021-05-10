0b8bba5882b7831224b9b1c7c14ac3a3c29bb0a9
AnnaKachkaeva <63503721+AnnaKachkaeva@users.noreply.github.com>
2021-05-10 13:19:06 +0300

    Delete src directory

--- src/main/java/com/anna/repositories/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.anna.repositories;
-
-import com.anna.domain.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends CrudRepository<Author, Long> {
-}
